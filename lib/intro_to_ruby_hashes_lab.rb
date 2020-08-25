78596c4f353580667eb379af38abbd6836debb54
Rodney Smith II <r_smith3@hotmail.com>
2020-08-25 05:45:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,9 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  
+  # return the correct value using the hash and key parameters   expect(read_from_hash({name: 'Steve'}, :name)).to eq('Steve')
+      expect(read_from_hash({'name' => 'Tzvi'}, 'name')).to eq('Tzvi')
 end
 
 def update_counting_hash(hash, key)
